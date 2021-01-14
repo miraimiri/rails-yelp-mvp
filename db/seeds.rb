ce4492904d903ac6285d6ac84aad9d73614a1a3f
Miriam Derenbach <miriam.derenbach@outlook.com>
2021-01-14 21:06:15 +0900

    finished simple app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning up DB"
+Restaurant.destroy_all
+puts "DB is clean"
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+
+puts "Done."
